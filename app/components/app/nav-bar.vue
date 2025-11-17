45345c62c19eb34ae8d8db3a2e023a7dd18385cd
Robin Braibant <robin.braibant@aplinedigital.be>
2025-11-17 20:33:42 +0100

    Added landing page

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">
+        Sign In With Github
+        <Icon name="tabler:brand-github-filled" size="20" /></a>
+    </div>
+  </div>
+</template>
